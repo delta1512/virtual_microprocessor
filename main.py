af9857d85b5c5d8ba6595434aed88e1920ca868d
delta1512 <markbel555@gmail.com>
2017-01-27 10:53:22 +1100

    Debugging and usability update
    Added debugging
    Added comments
    Added arguments
    Now the syntax is: python3.5 main.py [name of image - ".py"]

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
-import multiprocessing
+from sys import argv
+#import multiprocessing (might be used later on)
 import time
+import os
 import processor
 import memory
 
-global mem, cpu
+global mem, cpu, memsize
 mem = memory.mem()
 cpu = processor.cpu()
 
-terminal_debug = True
+debug = True
+memsize = 0xa00
 
-mem.initMem(0xa00)
+mem.initMem(memsize, argv[1])
 
-#def DOS(): #Default Output System
+def Debugger():
+    lines = []
+    DOSval = mem.read(0x0)
+    if type(DOSval) is int:
+        DOSval = hex(DOSval)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    #Default Debug
+    lines.append('Current: ' + hex(cpu.regdb[1][1]) + '\t' + str(mem.read(cpu.regdb[1][1])) + '\t|\t' + 'Memory info: ' + hex(memsize) + '(' + str(memsize) + ')Blocks\t' + 'DOS: ' + DOSval)
+    lines.append('-' * 119)
+
+    #CPU Debug
+    for reg in cpu.DEBUG():
+        if reg[0] == 'r':
+            for i, alt in enumerate(reg[1]):
+                lines.append('r' + str(i) + ':\t' + hex(alt) + '\t|')
+        else:
+            lines.append(reg[0] + ':\t' + hex(reg[1]) + '\t|')
+
+    #Memory Debug
+    lines[2] += mem.DEBUG(87)
+    while len(mem.stderr) > cpu.regcount:
+        del mem.stderr[0]
+    mem.stderr.reverse()
+    lnptr = 3
+    for x in range(0, cpu.regcount - 1):
+        if len(mem.stderr) != 0 and x < len(mem.stderr):
+            if x == 0:
+                indent = '> '
+            else:
+                indent = ' '
+            lines[lnptr] += indent + mem.stderr[x]
+            lnptr += 1
+        else:
+            break
+    mem.stderr.reverse()
+
+    #Print all the debugging information
+    for ln in lines:
+        print(ln)
+
+#MAIN LOOP
 mem_bus = None
-while mem.read(0x0) != 0xff:
-    if terminal_debug:
-        print('DEBUG: Current INS: ' + '\t' + hex(cpu.db[1][1]) + '\t' + str(mem.read(cpu.db[1][1])))
-        #print('DEBUG: CPU registers: ', cpu.DEBUG())
-    cpu_io = cpu.Tick(mem.read(cpu.db[1][1]), mem_bus)
-    if cpu_io != None:
-        mem_bus = eval(cpu_io)
+while mem.read(0x0) != 0xff: #While the halt value is not in the DOS
+    if debug:
+        Debugger()
+    cpu_io = cpu.Tick(mem.read(cpu.regdb[1][1]), mem_bus) #Tick the CPU
+    if cpu_io != None: #Handle the response
+        mem_bus = eval(cpu_io) #Takes python code and executes in main's scope
     else:
         mem_bus = None
-    time.sleep(0.1)
-
-'''
-response = cpu.Tick(mem.read(cpu.PC), None)
-if response != None:
-    cpu.Tick('lco 1', eval(response))
-'''
-#print(cpu.Tick(mem.read(cpu.PC), None))
-#print(cpu.Reg_Access('MBR', None))
+    time.sleep(1) #Clock speed
