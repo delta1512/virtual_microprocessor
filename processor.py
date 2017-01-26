a66e8612eda5af24efb619a0f167d3b629d9b06b
delta1512 <markbel555@gmail.com>
2017-01-27 10:56:39 +1100

    Cleaned up and added comments

--- processor.py
+++ processor.py
@@ -1,29 +1,23 @@
 class cpu:
     def __init__(self):
-        self.ACC = 0
-        self.PC = 1
-        self.MBR = 0
-        self.SP = 0
-
-        self.registers = [0, 0, 0]
-
-        self.db = [['ACC', self.ACC], ['PC', self.PC],
-                   ['MBR', self.MBR], ['SP', self.SP],
-                   ['r', self.registers]]
+        self.regcount = 7
+        self.regdb = [['ACC', 0], ['PC', 1],
+                   ['MBR', 0], ['SP', 0],
+                   ['r', [0, 0, 0]]]
 
     def Reg_Access(self, name, data):
         if name[:1] == 'r':
             if data == None:
-                return self.db[4][1][int(name[1])]
+                return self.regdb[4][1][int(name[1])]
             else:
-                self.db[4][1][int(name[1])] = data
+                self.regdb[4][1][int(name[1])] = data
                 return
-        for i, x in enumerate(self.db):
+        for i, x in enumerate(self.regdb):
             if name == x[0]:
                 if data == None:
                     return x[1]
                 else:
-                    self.db[i][1] = data
+                    self.regdb[i][1] = data
                     return
 
     def ALU(self, ins, src, dst):
@@ -60,7 +54,7 @@ class cpu:
         else:
             arg = interp
         if type(arg) is str:
-            for x in self.db:
+            for x in self.regdb:
                 if arg == x[0]:
                     arg = hex(self.Reg_Access(arg, None))
 
@@ -69,7 +63,7 @@ class cpu:
         if op == 'mrd':
             return 'mem.read(' + str(arg) + ')'
         if op == 'mwr':
-            return 'mem.write(' + str(arg) + ', ' + str(self.MBR) + ')'
+            return 'mem.write(' + str(arg) + ', ' + str(self.regdb[2][1]) + ')'
         if op == 'add':
             self.ALU('add', arga, argb)
         if op == 'sub':
@@ -81,15 +75,17 @@ class cpu:
         if op == 'jmp':
             self.Reg_Access('PC', int(arg, 16))
 
+    #Tick() returns the next instruction and result of any
+    #memory operations from the last tick
     def Tick(self, mem_at_PC, mem_requ):
-        if mem_requ != None:
-            self.db[2][1] = mem_requ
+        if mem_requ != None: #If the CPU requested memory, put it in the MBR
+            self.regdb[2][1] = mem_requ
         request = self.INS_Decoder(mem_at_PC)
-        self.db[1][1] += 1
-        return request
+        self.regdb[1][1] += 1
+        return request #Send a request to main
 
     def DEBUG(self):
-        return self.db
+        return self.regdb
 '''
 instruction set:
 
