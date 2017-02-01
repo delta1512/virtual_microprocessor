9de4f79629df34e7369fdd6a258463e646ea9d7f
delta1512 <markbel555@gmail.com>
2017-02-01 18:33:43 +1100

    Added size-checking and fixed exception when reading an integer

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 import processor
 import memory
 
-global mem, cpu, memsize
+global mem, cpu, memsize, columns
 mem = memory.mem()
 cpu = processor.cpu()
+rows, columns = os.popen('stty size', 'r').read().split()
 
 debug = True
-memsize = 0x100
+memsize = 0xa00
 
 mem.initMem(memsize, argv[1])
 
@@ -23,11 +24,14 @@ def Debugger():
 
     #Default Debug
     start = 'Current: ' + hex(cpu.regdb[1][1]) + '\t' + str(mem.read(cpu.regdb[1][1])) + '\t'
-    ins = mem.read(cpu.regdb[1][1])[:3]
-    if ins == 'mrd' or ins == 'mwr' or ins == 'lco' or ins == 'jmp':
+    if type(mem.read(cpu.regdb[1][1])) is int:
+        ins = mem.read(cpu.regdb[1][1])
+    else:
+        ins = mem.read(cpu.regdb[1][1])[:3]
+    if ins == 'mrd' or ins == 'mwr' or ins == 'lco' or ins == 'jmp': #Tabbing is still broken
         start += '\t'
     lines.append(start + '|\t' + 'Memory info: ' + hex(memsize) + '(' + str(memsize) + ')Blocks\t' + 'DOS: ' + DOSval)
-    lines.append('-' * 119)
+    lines.append('-' * int(columns))
 
     #CPU Debug
     for reg in cpu.DEBUG():
@@ -38,7 +42,7 @@ def Debugger():
             lines.append(reg[0] + ':\t' + hex(reg[1]) + '\t|')
 
     #Memory Debug
-    lines[2] += mem.DEBUG(86)
+    lines[2] += mem.DEBUG(50)
     while len(mem.stderr) > cpu.regcount:
         del mem.stderr[0]
     mem.stderr.reverse()
@@ -62,11 +66,11 @@ def Debugger():
 #MAIN LOOP
 mem_bus = None
 while mem.read(0x0) != 0xff: #While the halt value is not in the DOS
-    if debug:
-        Debugger()
     cpu_io = cpu.Tick(mem.read(cpu.regdb[1][1]), mem_bus) #Tick the CPU
     if cpu_io != None: #Handle the response
         mem_bus = eval(cpu_io) #Takes python code and executes in main's scope
     else:
         mem_bus = None
+    if debug:
+        Debugger()
     time.sleep(1) #Clock speed
